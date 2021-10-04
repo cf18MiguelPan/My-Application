dda7775e2906a2af756753d278d57e4e67574eed
Miguel Pan <cf18miguel.pan@iesjoandaustria.org>
2021-10-04 19:02:37 +0200

    hola1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //description
         Button enter = findViewById(R.id.enter);            //button
         EditText username = findViewById(R.id.username);    //username
         EditText password= findViewById(R.id.password);     //password
