dde3dc75e95335656fbea34b5e78a1624887ddc8
Miguel Pan <cf18miguel.pan@iesjoandaustria.org>
2021-10-18 19:43:21 +0200

    hola 22

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -24,10 +25,15 @@ public class MainActivity extends AppCompatActivity {
                 //username: admin && password: admin
                 if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
                     Log.i("Test","login ok");   //works
+                    openActivity2();
                 }else{
                     Log.i("Test","login ko");   //not works
                 }
             }
         });
+    }
+    public void openActivity2(){
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -1,14 +1,44 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        BottomNavigationView bottomNav = findViewById(R.id.main_menu);
+
+        //bottomNav.setOnItemSelectedListener(item -> {
+        bottomNav.setOnNavigationItemSelectedListener(item ->{
+            Fragment selectedFragment = null;
+            switch (item.getItemId()){
+                case R.id.home:
+                    selectedFragment = new fragmentHome();
+                    break;
+
+                case R.id.llistat:
+                    selectedFragment = new llistfragment();
+                    break;
+
+                case R.id.formulari:
+                    selectedFragment = new formufragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+            return true;
+        });
+
     }
 }
