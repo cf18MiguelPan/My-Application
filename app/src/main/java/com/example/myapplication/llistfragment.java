64541991b79138bea556d98fd63dcf0d69b406ad
Miguel Pan <cf18miguel.pan@iesjoandaustria.org>
2021-10-25 19:52:59 +0200

    hello'

--- app/src/main/java/com/example/myapplication/llistfragment.java
+++ app/src/main/java/com/example/myapplication/llistfragment.java
@@ -3,10 +3,14 @@ package com.example.myapplication;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -56,9 +60,20 @@ public class llistfragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_llistfragment, container, false);
+
+        View viewList = inflater.inflate(R.layout.activity_llista,container,false);
+
+        ArrayList<String> array_noms = new ArrayList<String>();
+        array_noms.add(new String("Hola"));
+
+        RecyclerView recyclerView = viewList.findViewById(R.id.recyclerView);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(array_noms);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        return viewList;
+
     }
 }
